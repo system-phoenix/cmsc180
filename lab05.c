1a026dad69a24c6d874bc8d25e3fe6d3f5888b77
aadevera <aadevera@up.edu.ph>
2016-11-07 16:31:20 +0800

    recall scatterv

--- lab05.c
+++ lab05.c
@@ -40,6 +40,9 @@ void broadcast(int matrix[n][n], int rank) {
         }
         // int MPI_Scatterv(void *sendbuf, int *sendcounts, int *displs, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, int root, MPI_Comm comm);
         MPI_Scatterv(matrix, sendCounts, displs, MPI_INT, m, recvcount, MPI_INT, 0, MPI_COMM_WORLD);
+        if(rank == 0) {
+                MPI_Scatterv(matrix, sendCounts, displs, MPI_INT, m, recvcount, MPI_INT, 0, MPI_COMM_WORLD);
+        }
 }
 
 void column_sum(int matrix[n][n]) {
