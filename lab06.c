e11ad883e8291c1f7c6efef2a3b94b56fb11d98a
system-phoenix <dmorias@up.edu.ph>
2016-12-04 02:09:04 +0800

    Uses the mathematical algorithm properly.

--- lab06.c
+++ lab06.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <time.h>
-#define n 4
+#define n 5
 
 void printMatrix(int *matrix, int row, int col, int last) {
     printf("==================================\n");
@@ -79,8 +79,27 @@ void matrix_divide(int ** A, int ay, int ax, int B) {
     }
 }
 
+int void_columns(int * A, int * B) {
+    int i, j, squares = 0;
+    for(i = 0; i < n; i++) {
+        if((A)[i * n + i]) {
+            for(j = 0; j < n; j++) {
+                if(i == j) continue;
+                else {
+                    if((B)[i * n + j]) {
+                        (A)[j * n + j] = 0;
+                    }
+                }
+            }
+
+            squares += (A)[i * n + i];
+        }
+    }
+    return squares;
+}
+
 int main(int argc, char** argv) {
-    int rank, p;
+    int rank, p, squares;
     int i = 0;
     int displ = 0;
     int *absMatrix = NULL;
@@ -110,45 +129,31 @@ int main(int argc, char** argv) {
         for(i = 0; i < n * n; i++) {
             absMatrix[i] = rand() % 2;                 //randomize value for the matrix
         }
-        transform(&absMatrix, n, n);
+        // transform(&absMatrix, n, n);
 
-        // absMatrix[0] = 0;   absMatrix[1] = 1;   absMatrix[2] = 1;   absMatrix[3] = 1;   absMatrix[4] = 1;
+        // printMatrix(absMatrix, n, n, 0);
 
-        // absMatrix[5] = 1;   absMatrix[6] = 0;   absMatrix[7] = 1;   absMatrix[8] = 1;   absMatrix[9] = 1;
+        // matrix_multiply(&relMatrix, &absMatrix, n, n, &absMatrix, n, n);
+        // temp = relMatrix;
+        // matrix_multiply(&relMatrix, &temp, n, n, &absMatrix, n, n);
+        // temp = relMatrix;
+        // matrix_multiply(&relMatrix, &temp, n, n, &absMatrix, n, n);
+        // matrix_divide(&relMatrix, n, n, 8);
 
-        // absMatrix[10] = 1;  absMatrix[11] = 1;  absMatrix[12] = 0;  absMatrix[13] = 1;  absMatrix[14] = 1;
+        // squares = void_columns(relMatrix, absMatrix);
 
-        // absMatrix[15] = 1;  absMatrix[16] = 1;  absMatrix[17] = 1;  absMatrix[18] = 0;  absMatrix[19] = 1;
-
-        // absMatrix[20] = 1;  absMatrix[21] = 1;  absMatrix[22] = 1;  absMatrix[23] = 1;  absMatrix[24] = 0;
-
-
-        absMatrix[0] = 0;   absMatrix[1] = 1;   absMatrix[2] = 0;   absMatrix[3] = 1;
-
-        absMatrix[4] = 1;   absMatrix[5] = 0;   absMatrix[6] = 1;   absMatrix[7] = 1;
-
-        absMatrix[8] = 0;   absMatrix[9] = 1;   absMatrix[10] = 0;  absMatrix[11] = 1;
-
-        absMatrix[12] = 1;  absMatrix[13] = 1;  absMatrix[14] = 1;  absMatrix[15] = 0;
-
-
-        printMatrix(absMatrix, n, n, 0);
-
-        matrix_multiply(&relMatrix, &absMatrix, n, n, &absMatrix, n, n);
-        temp = relMatrix;
-        matrix_multiply(&relMatrix, &temp, n, n, &absMatrix, n, n);
-        temp = relMatrix;
-        matrix_multiply(&relMatrix, &temp, n, n, &absMatrix, n, n);
-        // for(i = 0; i < p; i++) {
-        //     displs[i] = displ;
-        //     sendCounts[i] = round((float) (n / p) + i * (n / p)) - round((float) i * (n / p));
-        //     displ += sendCounts[i];
-        // }
+        // printMatrix(relMatrix, n, n, 0);
+        // printf("Number of squares: %d\n", squares);
+        // // for(i = 0; i < p; i++) {
+        // //     displs[i] = displ;
+        // //     sendCounts[i] = round((float) (n / p) + i * (n / p)) - round((float) i * (n / p));
+        // //     displ += sendCounts[i];
+        // // }
     }
 
     // begin = clock();
 
-    // MPI_Scatterv(absMatrix, sendCounts, displs, MPI_INT, relMatrix, n * n / p, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&absMatrix, n, MPI_INT, 0, MPI_COMM_WORLD);
 
     // for(i = n / p; i < n * n / p; i++) {
     //     relMatrix[i] = relMatrix[i] + relMatrix[i - n / p];
